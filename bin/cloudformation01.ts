a02dc5b8b45746511f7c96ee3d33f4ea0f25d4f0
armandotapiach <armando.tapia@kushkipagos.com>
2024-04-07 12:27:41 -0500

    Initial commit

--- bin/cloudformation01.ts
+++ bin/cloudformation01.ts
@@ -1,21 +1,30 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { Cloudformation01Stack } from '../lib/cloudformation01-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { Cloudformation01Stack } from "../lib/cloudformation01-stack";
 
 const app = new cdk.App();
-new Cloudformation01Stack(app, 'Cloudformation01Stack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new Cloudformation01Stack(app, "Cloudformation01Stack", {
+  env: {
+    account: "381492260472",
+    region: "us-east-1",
+  },
 });
+const props = {
+  name: app.node.tryGetContext("name"),
+  domainName: app.node.tryGetContext("domainName"),
+  applicationTag: app.node.tryGetContext("applicationTag"),
+  hostedZoneName: app.node.tryGetContext("hostedZone"),
+  sourcePath: app.node.tryGetContext("sourcePath"),
+  env: {
+    account: app.node.tryGetContext("accountId"),
+    region: app.node.tryGetContext("region"),
+  },
+  crossRegionReferences: true,
+};
+
+new Cloudformation01Stack(app, props.applicationTag, {
+  ...props,
+});
+
+app.synth();
--- lib/cloudformation01-stack.ts
+++ lib/cloudformation01-stack.ts
@@ -1,16 +1,72 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  CfnDistribution,
+  CfnOriginAccessControl,
+  CloudFrontAllowedCachedMethods,
+  CloudFrontAllowedMethods,
+  CloudFrontWebDistribution,
+  ViewerProtocolPolicy,
+} from "aws-cdk-lib/aws-cloudfront";
+import { BlockPublicAccess, Bucket, BucketEncryption } from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+
+export interface InfraStackProps extends cdk.StackProps {
+  name: string;
+  domainName: string;
+  sourcePath: string;
+}
 
 export class Cloudformation01Stack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: InfraStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucketName = `${props.name}-bucket`;
+    const contentBucket = new Bucket(this, "StaticWebsiteBucket", {
+      bucketName: bucketName,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      encryption: BucketEncryption.S3_MANAGED,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Cloudformation01Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    if (props.sourcePath !== undefined) {
+      new BucketDeployment(this, "UploadContent", {
+        sources: [Source.asset(props.sourcePath)],
+        destinationBucket: contentBucket,
+      });
+    }
+
+    const oac = new CfnOriginAccessControl(this, "OAC", {
+      originAccessControlConfig: {
+        name: `${props.name}-oac`,
+        originAccessControlOriginType: "s3",
+        signingBehavior: "always",
+        signingProtocol: "sigv4",
+      },
+    });
+
+    const cloudFrontDistribution = new CloudFrontWebDistribution(this, "StaticWebsiteDistribution", {
+      originConfigs: [
+        {
+          s3OriginSource: { s3BucketSource: contentBucket },
+          behaviors: [
+            {
+              isDefaultBehavior: true,
+              allowedMethods: CloudFrontAllowedMethods.ALL,
+              compress: true,
+              cachedMethods: CloudFrontAllowedCachedMethods.GET_HEAD,
+              viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+              minTtl: cdk.Duration.seconds(0),
+              maxTtl: cdk.Duration.seconds(86400),
+              defaultTtl: cdk.Duration.seconds(3600),
+            },
+          ],
+        },
+      ],
+    });
+
+    const cfnDistribution = cloudFrontDistribution.node.defaultChild as CfnDistribution;
+    cfnDistribution.addPropertyOverride("DistributionConfig.Origins.0.OriginAccessControlId", oac.getAtt("Id"));
   }
 }
